797bc132f41e8bbb5a4904e6407719f41d8a9541
TobyRas1992 <tobiasrasmussen@outlook.com>
2021-09-11 08:34:27 +0200

    added input fields to update form

--- src/app/update-view/update-view.component.ts
+++ src/app/update-view/update-view.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+// Fetch API
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+// Angular Material
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-update-view',
@@ -7,14 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateViewComponent implements OnInit {
 
-  constructor() { }
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' }
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<UpdateViewComponent>,
+  ) { }
 
   ngOnInit(): void {
   }
 
-  /* cancel(): void {
-    this.router.navigate(['/profile']).then(() => {
-      window.location.reload();
+  updateUserInfo(): void {
+    this.fetchApiData.editUser(this.userData).subscribe((response) => {
+      this.dialogRef.close();
+      localStorage.setItem('user', response.Username);
+      this.snackBar.open('Profile updated', 'OK', {
+        duration: 2000,
+      });
+    }, (response) => {
+      this.snackBar.open(response, 'OK', {
+        duration: 2000,
+      });
     });
-  } */
+    setTimeout(function () {
+      window.location.reload();
+    }, 1250);
+  }
+
+  cancel(): void {
+    this.dialogRef.close();
+  }
+
 }
